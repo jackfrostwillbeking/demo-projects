f5457879e65a0c9e5cc0cbc4200363ffcdc27289
kawasakiya <31604636+jackfrostwillbeking@users.noreply.github.com>
2018-08-06 19:25:21 +0900

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, Response
-from processor.simple_streamer import Processor
+#from processor.simple_streamer import Processor
 #from processor.pedestrian_detector import Processor
-# from processor.motion_detector import Processor
+from processor.motion_detector import Processor
 #from processor.people_counter import PeopleCounter as Processor
 #from processor.person_detector import Processor
 import time
